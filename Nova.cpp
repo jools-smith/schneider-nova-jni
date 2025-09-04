5e5be627f442197cd2ce3d89ba44747c04f3cdfb
jools <julian.smith@flexera.com>
2025-09-04 09:59:10 +0100

    Added FNE toolkit versioning

--- Nova.cpp
+++ Nova.cpp
@@ -9,6 +9,7 @@
 #define LIB_EXPORT __attribute__ ((visibility ("default")))
 
 #include <ctime>
+#include <time.h>
 #include <string>
 #include <sstream>
 #include <set>
@@ -17,10 +18,10 @@
 using namespace std;
 
 #include "dump.h"
-
+#include "FneHelpers.h"
 #include "Nova.h"
 #include "JNIHelper.h"
-
+#include "IdentityClient.h"
 #include "TraWrapper.h"
 
 #include "UserData.h"
@@ -33,6 +34,9 @@ using namespace std;
  */
 
 static TraWrapper tra;
+
+static const string datestamp = __DATE__;
+static const string timestamp = __TIME__;
 
 extern "C" {
 
@@ -102,6 +106,63 @@ int do_initialize_fail(tra_Data*p) {
 	}
 }
 
+int cf_tamper_detected(tra_Data*p) {
+    DEBUG_PRINTLN("cf_tamper_detected");
+
+	try {
+		UserData*const pud = static_cast<UserData*>(tra_get_user_data(p));
+        DEBUG_PRINT("user data %p", (void*)pud);
+
+	    return  1;
+	}
+	catch (...) {
+	    DEBUG_PRINTLN("exception");
+	    return -1;
+	}
+}
+
+LIB_EXPORT jboolean JNICALL Java_com_flexera_schneider_fnesigner_Nova_version(JNIEnv *env, jobject object) {
+
+	DEBUG_PRINTLN("Java_com_flexera_schneider_fnesigner_Nova_version")
+
+    const JNIHelper jvm(env, object);
+
+    try {
+    	ErrorWrapper error;
+
+    	Status status(error);
+    	status["FlcErrorCreate"] = FlcErrorCreate(error);
+
+    	LicensingWrapper licensing;
+    	status["FlcLicensingCreate"] = FlcLicensingCreate(licensing, identity_data, sizeof identity_data, nullptr, nullptr, error);
+
+    	const FlcChar*fneVersion;
+    	status = FlcGetClientVersion(licensing, &fneVersion, error);
+
+        jvm.set_string_field("fneToolkitVersion", fneVersion);
+
+    	struct tm time;
+    	strptime(datestamp.c_str(), "%b %e %Y", &time);
+
+    	char bfr[90];
+
+    	strftime(bfr, sizeof bfr, "%Y-%m-%d", &time);
+
+        jvm.set_string_field("nativeLibraryVersion", string(bfr) + " " + timestamp);
+
+		return JNI_TRUE;
+    }
+    catch (const runtime_error& err) {
+
+        jvm.set_string_field("message", err.what());
+
+        cout << "exception | " << err.what() << endl;
+
+        return JNI_FALSE;
+    }
+}
+
+
 LIB_EXPORT jboolean JNICALL Java_com_flexera_schneider_fnesigner_Nova_process(JNIEnv *env, jobject object) {
 
 	DEBUG_PRINTLN("Java_com_flexera_schneider_fnesigner_Nova_process")
@@ -117,29 +178,11 @@ LIB_EXPORT jboolean JNICALL Java_com_flexera_schneider_fnesigner_Nova_process(JN
 
     	/** set up the JNI fields **/
 
-//    	string fiield_name = "RSA256";
-//    	string fiield_value = "HMAC";
-//
-//    	fiield_name = tra_get_string(tra, TRA_STRING_identity_field_name_ALIAS_3);
-//    	fiield_value = tra_get_string(tra, TRA_STRING_identity_ALIAS_7);
-
-//        jvm.set_string_field(
-//        		tra_get_string(tra, TRA_STRING_identity_field_name_ALIAS_3),
-//				tra_get_string(tra, TRA_STRING_identity_ALIAS_7));
-
-
         jvm.set_string_field(
         		tra_get_string(tra, userdata.identity.name),
 				tra_get_string(tra, userdata.identity.value));
 
-//        jvm.set_string_field(fiield_name, fiield_value);
-
-//        fiield_name = tra_get_string(tra, TRA_STRING_message_field_name_ALIAS_1);
-//        fiield_value =  tra_get_string(tra, TRA_STRING_ok_ALIAS_2);
-
-//        jvm.set_string_field(
-//        		tra_get_string(tra, TRA_STRING_message_field_name_ALIAS_1),
-//				tra_get_string(tra, TRA_STRING_ok_ALIAS_2));
+        tra_release_string(tra, userdata.identity.value);
 
         jvm.set_string_field(
         		tra_get_string(tra, userdata.message.name),
@@ -157,140 +200,5 @@ LIB_EXPORT jboolean JNICALL Java_com_flexera_schneider_fnesigner_Nova_process(JN
     }
 }
 
-//jboolean JNICALL Java_com_flexera_schneider_fnesigner_Nova_process_x(JNIEnv *env, jobject object) {
-//    TFT status(tra, TRA_VARIABLE_minus_one_ALIAS_1); //-2
-//    TFT one(tra, TRA_VARIABLE_one_ALIAS_3);
-//
-//    status -= one;
-//    if (env) {
-//        UserData userdata(tra);
-//
-////            userdata.path = convert(env, licenseFilePath);
-//
-//        // AX = 0
-//        tra_set_value(tra, TRA_VARIABLE_ax_ALIAS_1, TRA_STRING_identity_ALIAS_1);
-//        tra_set_value(tra, TRA_VARIABLE_bx_ALIAS_1, TRA_STRING_identity_bad_ALIAS_1);
-//
-//        /**
-//         * For now ther is no license check
-//         *
-//         * tra_if(tra, TRA_SNIF_initialize_ALIAS_1, &userdata);
-//         *
-//         */
-//        tra_if(tra, TRA_SNIF_initialize_ALIAS_1, &userdata);
-//        status += TFT(tra, TRA_VARIABLE_one_ALIAS_4); // -1
-//        jclass source = env->GetObjectClass(object);
-//        if (source) {
-//            DEBUG_PRINT("### class %p\n", (void* )source)
-//
-//            jfieldID field_m = env->GetFieldID(source, "message", "Ljava/lang/String;");
-//
-//            DEBUG_PRINT("### field_m %p\n", (void* )field_m)
-//
-//            if (field_m) {
-//                jstring string = env->NewStringUTF(userdata.error.c_str());
-//                if (string) {
-//                    DEBUG_PRINT("### message %p\n", (void* )string)
-//
-//                    env->SetObjectField(object, field_m, string);
-//                    status += TFT(tra, TRA_VARIABLE_one_ALIAS_5); // 0
-//                }
-//            }
-//
-//            jfieldID field_i = env->GetFieldID(source, "identity", "Ljava/lang/String;");
-//
-//            DEBUG_PRINT("### field_i %p\n", (void* )field_i)
-//
-//            if (field_i) {
-//                int alias = 0;
-//                tra_get_value(tra, TRA_VARIABLE_status_ALIAS_17, &alias);
-//
-//                string id = tra_get_string(tra, alias);
-//
-//                DEBUG_PRINT("### %s\n", id.c_str())
-//
-//                jstring identity = env->NewStringUTF(id.c_str());
-//                if (identity) {
-//                    DEBUG_PRINT("### identity %p\n", (void* )identity);
-//
-//                    env->SetObjectField(object, field_i, identity);
-//
-//                    status += TFT(tra, TRA_VARIABLE_one_ALIAS_6); // + 1
-//                }
-//            }
-//        }
-//    }
-//
-//    DEBUG_PRINT("RETURN %i %i %s\n", status.get(), one.get(), status == one ? "T" : "F");
-//
-//    return status == one;
-//}
-
-//bool LIB_EXPORT test_tra(stringstream &stream) {
-//    TFT status(tra);
-//
-//    tra_copy(tra, TRA_VARIABLE_status_ALIAS_1, TRA_VARIABLE_zero_ALIAS_1);
-//
-//    // = 0
-//    int count = 144;
-//
-//    tra_call(tra, TRA_SF_ADD_ALIAS_1, NULL, TRA_VARIABLE_status_ALIAS_2, TRA_VARIABLE_one_ALIAS_1, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_2, NULL, TRA_VARIABLE_status_ALIAS_3, TRA_VARIABLE_one_ALIAS_2, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_3, NULL, TRA_VARIABLE_status_ALIAS_4, TRA_VARIABLE_one_ALIAS_3, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_1, NULL, TRA_VARIABLE_status_ALIAS_5, TRA_VARIABLE_one_ALIAS_4, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_2, NULL, TRA_VARIABLE_status_ALIAS_6, TRA_VARIABLE_one_ALIAS_5, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_3, NULL, TRA_VARIABLE_status_ALIAS_7, TRA_VARIABLE_one_ALIAS_6, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_1, NULL, TRA_VARIABLE_status_ALIAS_8, TRA_VARIABLE_one_ALIAS_7, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_2, NULL, TRA_VARIABLE_status_ALIAS_9, TRA_VARIABLE_one_ALIAS_8, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_3, NULL, TRA_VARIABLE_status_ALIAS_10, TRA_VARIABLE_one_ALIAS_9, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_1, NULL, TRA_VARIABLE_status_ALIAS_11, TRA_VARIABLE_one_ALIAS_10, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_2, NULL, TRA_VARIABLE_status_ALIAS_12, TRA_VARIABLE_one_ALIAS_11, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_3, NULL, TRA_VARIABLE_status_ALIAS_13, TRA_VARIABLE_one_ALIAS_12, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_1, NULL, TRA_VARIABLE_status_ALIAS_14, TRA_VARIABLE_one_ALIAS_13, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_2, NULL, TRA_VARIABLE_status_ALIAS_15, TRA_VARIABLE_one_ALIAS_14, NULL);
-//    tra_call(tra, TRA_SF_ADD_ALIAS_3, NULL, TRA_VARIABLE_status_ALIAS_16, TRA_VARIABLE_one_ALIAS_15, NULL);
-//    // 15
-//
-//    tra_call(tra, TRA_SF_SUBTRACT_ALIAS_1, NULL, TRA_VARIABLE_status_ALIAS_2, TRA_VARIABLE_one_ALIAS_1, NULL);
-//    tra_call(tra, TRA_SF_SUBTRACT_ALIAS_2, NULL, TRA_VARIABLE_status_ALIAS_3, TRA_VARIABLE_one_ALIAS_2, NULL);
-//    tra_call(tra, TRA_SF_SUBTRACT_ALIAS_3, NULL, TRA_VARIABLE_status_ALIAS_4, TRA_VARIABLE_one_ALIAS_3, NULL);
-//    // 12
-//
-//    tra_call(tra, TRA_SF_MULTIPLY_ALIAS_3, NULL, TRA_VARIABLE_status_ALIAS_1, TRA_VARIABLE_status_ALIAS_2, NULL);
-//    // 144
-//
-//    TFT result(tra, TRA_VARIABLE_status_ALIAS_3);
-//
-//    stream << "Expected count:" << count << " - actual count:" << result.get();
-//
-//    return count == result;
-//}
-//
-//bool LIB_EXPORT test_fne(const string &licenseFilePath, stringstream &stream) {
-//    UserData data(tra);
-//
-//    data.path = licenseFilePath;
-//
-//    TFT status(tra);
-//    const TFT istrue(tra, TRA_VARIABLE_one_ALIAS_19);
-//    status = data.test_initialize();
-//    if (status == istrue) {
-//        status = data.test_dump(stream);
-//    }
-//
-//    return status == istrue;
-//}
-//
-////
-//bool LIB_EXPORT quick_test_tra() {
-//    DEBUG_PRINTLN("TFT status(tra)")
-//    TFT status(tra);
-//
-//    DEBUG_PRINTLN("const TFT istrue(tra, TRA_VARIABLE_one_ALIAS_19)")
-//    const TFT istrue(tra, TRA_VARIABLE_one_ALIAS_19);
-//
-//    DEBUG_PRINTLN("return status == istrue")
-//    return status == istrue;
-//}
 }
 /* extern c */
--- UserData.h
+++ UserData.h
@@ -9,8 +9,6 @@
 #include "FlcLicenseManager.h"
 #include "FlcFeature.h"
 
-#include "fnedemo.RSA512.IdentityClient.h"
-
 #include <string>
 #include <vector>
 #include <sstream>
@@ -24,45 +22,20 @@ public:
 	FieldWrapper(TraWrapper &value) : name(value, TRA_VARIABLE_minus_one_ALIAS_2), value(value, TRA_VARIABLE_minus_one_ALIAS_3) {
 
 	}
-//
-//	int&name() {
-//		return m_name;
-//	}
-//	int&value() {
-//		return m_value;
-//	}
 };
 
-class UserData {
+class UserData final {
     TraWrapper &m_tra;
 
-    FlcErrorRef m_error;
-    FlcLicensingRef m_licensing;
-    FlcLicenseRef m_license;
-
 public:
-    std::string error;
-    std::string path;
-
     FieldWrapper identity;
     FieldWrapper message;
     TFT status;
 
-    UserData(TraWrapper &value) : m_tra(value), m_error(0), m_licensing(0), m_license(0), identity(value), message(value), status(value, TRA_VARIABLE_minus_one_ALIAS_1){
+    UserData(TraWrapper &value) : m_tra(value), identity(value), message(value), status(value, TRA_VARIABLE_minus_one_ALIAS_1){
     }
 
-    virtual ~UserData();
-
-    /**
-     * for external testing only - not part of JNI functionality
-     */
-    int test_initialize();
-
-    /**
-     * for external testing only - not part of JNI functionality
-     */
-    int test_dump(std::stringstream &stream);
-
+    ~UserData() = default;
 };
 
 #endif
